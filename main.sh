3c55c07199e02e7c62699889393a31dc53c92fb5
Robin Hellmers <robin@hellmers.se>
2024-09-09 13:03:21 +0200

    Remove submodule bash-lib-handle-input

--- main.sh
+++ main.sh
@@ -29,7 +29,6 @@ library_sourcing()
 
     ### Source libraries ###
     source "$LIB_PATH/lib_core.bash" || exit 1
-    source_lib "$LIB_PATH/lib_handle_input.bash"
     source_lib "$LIB_PATH/lib.bash"
 }
 
